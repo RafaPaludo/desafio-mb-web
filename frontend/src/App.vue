c2d738bd569f575ef2196290ba258b76b20a25d3
RafaPaludo <rafael.ricardopp@gmail.com>
2025-06-12 01:52:05 -0300

    feat: initialize frontend application

---
+++ frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <PageRegistration />
+  </main>
+</template>
+
+<script setup>
+import PageRegistration from '@/views/PageRegistration.vue';
+</script>
+
+<style lang="scss">
+main {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 4rem 0;
+}
+</style>
