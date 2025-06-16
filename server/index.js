8ac8269c135b727158f30d4456119f444a72c43d
RafaPaludo <rafael.ricardopp@gmail.com>
2025-06-15 21:50:41 -0300

    feat: implement POST validation on server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
+const distPath = path.resolve(__dirname, '../frontend/dist');
+const validateUser = require('./validators/useValidator');
 
 const app = express();
 const port = 3000;
 
-const distPath = path.resolve(__dirname, '../frontend/dist');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.redirect('/registration');
@@ -17,7 +20,15 @@ app.get('/registration', (req, res) => {
 });
 
 app.post('/registration', (req, res) => {
-  console.log("POST")
+  try {
+    const { ok } = validateUser(req.body);
+
+    if (ok) res.status(200).json({ message: 'Registro feito com sucesso!' });
+  } catch (error) {
+    if (error.message) return res.status(400).json({ message: error.message });
+
+    res.status(500).json({ message: 'Erro interno do servidor' });
+  }
 });
 
 app.listen(port, () => {
---
+++ server/validators/useValidator.js
@@ -0,0 +1,102 @@
+const validateUser = (body = {}) => {
+  console.log("Entrou no validator, body:::", body);
+
+  const commonFields = [
+    {
+      property: "email",
+      name: "E-mail",
+      typeof: "string",
+      length: 1
+    },
+    {
+      property: "password",
+      name: "Senha",
+      typeof: "string",
+      length: 8
+    },
+    {
+      property: "selectedPersonType",
+      name: "Tipo de pessoa",
+      typeof: "string",
+      length: 2
+    }
+  ];
+  const fieldsModel = {
+    pf: [
+      ...commonFields,
+      {
+        property: "name",
+        name: "Nome",
+        typeof: "string",
+        length: 1
+      },
+      {
+        property: "cpf",
+        name: "CPF",
+        typeof: "string",
+        length: 11
+      },
+      {
+        property: "birthday",
+        name: "Data de nascimento",
+        typeof: "string",
+        length: 11
+      },
+      {
+        property: "phone",
+        name: "Telefone",
+        typeof: "string",
+        length: 11
+      }
+    ],
+    pj: [
+      ...commonFields,
+      {
+        property: "companyName",
+        name: "Nome da empresa",
+        typeof: "string",
+        length: 1
+      },
+      {
+        property: "cnpj",
+        name: "CNPJ",
+        typeof: "string",
+        length: 11
+      },
+      {
+        property: "openingDate",
+        name: "Data de abertura",
+        typeof: "string",
+        length: 11
+      },
+      {
+        property: "companyPhone",
+        name: "Telefone da empresa",
+        typeof: "string",
+        length: 11
+      }
+    ]
+  };
+  const selectedPersonType = body?.selectedPersonType?.toLowerCase();
+  const fields = fieldsModel[selectedPersonType];
+
+  if (!fields) throw new Error("Tipo de usuário não informado. Revise os campos e tente novamente.");
+
+  const malformedFields = fields.reduce((acc, field) => {
+    if (
+      !body.hasOwnProperty(field.property) ||
+      typeof body[field.property] !== field.typeof ||
+      body[field.property].trim().length < field.length
+    ) acc.push(field.name);
+
+    return acc;
+  }, []).join(", ");
+  
+  if (malformedFields) {
+    throw new Error(`Os seguintes campos não foram preenchidos corretamente: ${malformedFields}.`);
+  }
+
+  return { ok: true };
+};
+
+module.exports = validateUser;
